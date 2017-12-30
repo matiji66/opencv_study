5052cb7a4f64b195207805b427fb35d5d9591f7a
scott huang <scotthuang1989@163.com>
2017-12-30 21:32:21 +0800

    update main

--- opencv_cpp/image_datatype/main.cpp
+++ opencv_cpp/image_datatype/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 using namespace cv;
+using namespace std;
 
 
 void create_array()
@@ -37,8 +38,49 @@ void access_by_iterator()
   std::cout << max  << std::endl;
 }
 
+const std::string image_file_path = "/home/scott/Pictures/me.jpg";
+
+int convert_to_CV_32FC1()
+{
+  /*
+  Mat.convertTo: only convert the type. the value is not sacled properly.
+  Eg:
+  at location (3,3). when data type is uint8. the value is 13
+  after convert to float32, the value is still 13. so, image can't display correctly.
+
+  */
+  cv::Mat img = cv::imread(image_file_path, CV_LOAD_IMAGE_GRAYSCALE);
+  // cout<< CV_LOAD_IMAGE_ANYDEPTH << CV_LOAD_IMAGE_COLOR << CV_LOAD_IMAGE_GRAYSCALE<<endl;
+  if (img.empty())
+  {
+    std::cout << "open file failed"<<std::endl;
+    return -1;
+  }
+  cv::namedWindow("Example1", cv::WINDOW_AUTOSIZE);
+  cv::imshow("Example1", img);
+  cv::waitKey();
+  cout << " R Type: "<< img.type()<< endl;
+  cout << "at <3,3>: "<< int(img.at<uchar>(3,3)) << endl;
+
+  cv::Mat img2;
+  img.convertTo(img2, CV_32FC1);
+  cout << "after conversion: "<<img2.at<float>(3,3) << endl;
+  cv::imshow("Example1", img2);
+  cv::waitKey();
+  cout << "after conversion channel count: " << img2.channels()<<" Type: " << img2.type() << endl;
+
+  cv::Mat img3;
+  img2.convertTo(img3, CV_8UC1);
+  cout << "after conversion 22: "<<int(img3.at<uchar>(3,3)) << endl;
+  cv::imshow("Example1", img3);
+  cv::waitKey();
+
+  cv::destroyWindow("Example1");
+  return 0;
+}
+
 int main(int argc, char** argv )
 {
-  access_by_iterator();
+  convert_to_CV_32FC1();
   return 0;
 }
